98cc7a8ca135d55d3e8cc0ce3329fa70c3254f82
chuck francis <chuck_francis10@yahoo.com>
2020-01-30 20:40:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StillPositive.sln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gotta stay positive!");
+            Console.WriteLine( "389, 26, 712, 94, 868, 227, 422, 259, 278, 799");
+            
+        }
+    }
+}
